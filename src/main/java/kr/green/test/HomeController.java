20754d6fc08a2cf38b86b2acb5c26e504c3b663e
joyesung <frog0039@naver.com>
2019-08-05 11:18:27 +0900

    Mybatice 테스트완료

--- src/main/java/kr/green/test/HomeController.java
+++ src/main/java/kr/green/test/HomeController.java
@@ -6,21 +6,28 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.green.test.service.MemberService;
+import kr.green.test.vo.MemberVO;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+	@Autowired
+	MemberService memberservice;
 	
 	@RequestMapping(value= "/")
 	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
+	    MemberVO mVo = memberservice.getMember("frog9912");
+		System.out.println(mVo);
 	    mv.setViewName("/main/home");
 	    mv.addObject("setHeader", "타일즈");
 	    return mv;
